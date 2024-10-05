342f71433039d59786302c02c11defcd33f2583c
Jose Ysique <43045946+jysique@users.noreply.github.com>
2024-10-04 20:40:45 -0500

    attributes

---
+++ Assets/Attributes/HelpBox/HelpBoxAttribute.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+ 
+public enum HelpBoxMessageType { None, Info, Warning, Error }
+ 
+public class HelpBoxAttribute : PropertyAttribute {
+ 
+    public string text;
+    public HelpBoxMessageType messageType;
+    public float height;
+ 
+    public HelpBoxAttribute(string text, HelpBoxMessageType messageType = HelpBoxMessageType.None, float height = 0) {
+        this.text = text;
+        this.messageType = messageType;
+        this.height = height;
+    }
+}
